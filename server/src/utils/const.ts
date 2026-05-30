59ddea2c52e96018ee560d77a4fdb9d7c4fa8cd0
Abhijit <abhijitkumar1859@gmail.com>
2026-05-30 23:07:38 +0530

    more words added

--- server/src/utils/const.ts
+++ server/src/utils/const.ts
@@ -1,6 +1,100 @@
 export const MAX_ROUNDS = 1
-export const WORDS = ["elephant", "guitar", "volcano", "umbrella", "penguin"]
+
+export const WORDS = [
+   
+  "elephant",
+  "penguin",
+  "lion",
+  "tiger",
+  "monkey",
+  "giraffe",
+  "rabbit",
+  "duck",
+  "fish",
+  "cat",
+  "dog",
+  "horse",
+  "cow",
+
+   
+  "pizza",
+  "burger",
+  "ice cream",
+  "banana",
+  "apple",
+  "cake",
+  "donut",
+  "hotdog",
+  "watermelon",
+  "sandwich",
+
  
+  "guitar",
+  "umbrella",
+  "clock",
+  "chair",
+  "table",
+  "lamp",
+  "phone",
+  "camera",
+  "key",
+  "backpack",
+  "bicycle",
+  "toothbrush",
+  "scissors",
+
+  
+  "volcano",
+  "mountain",
+  "tree",
+  "sun",
+  "moon",
+  "rainbow",
+  "cloud",
+  "river",
+  "flower",
+  "cactus",
+
+   
+  "car",
+  "bus",
+  "train",
+  "airplane",
+  "rocket",
+  "helicopter",
+  "boat",
+  "submarine",
+
+  
+  "school",
+  "hospital",
+  "castle",
+  "beach",
+  "park",
+  "zoo",
+
+  // Sports
+  "football",
+  "basketball",
+  "cricket",
+  "tennis",
+  "baseball",
+
+  
+  "robot",
+  "ghost",
+  "pirate",
+  "superhero",
+  "crown",
+  "treasure",
+  "diamond",
+  "gift",
+  "balloon",
+  "trophy"
+]
+
 export function getRandomWords() {
-  return [...WORDS].sort(() => Math.random() - 0.5).slice(0, 3)
+  return [...WORDS]
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 3)
 }
